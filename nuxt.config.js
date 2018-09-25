76211174e69a23a7b02597bfbcb1c2c18ebe8b5e
David Weldon <dweldon@gmail.com>
2018-09-24 17:02:41 -0700

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
+  mode: 'spa',
+};
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello {{ message }}!</h1>
+</template>
+
+<script>
+const obj = { 0: 'Nuxt' };
+
+export default {
+  computed: {
+    message() {
+      return Object.entries(obj)[0][1];
+    }
+  }
+};
+</script>
